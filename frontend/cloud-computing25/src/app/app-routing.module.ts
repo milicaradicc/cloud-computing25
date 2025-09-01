5917b56c5bc556b66db61e7d7cdaef4a4d300bd7
Mijat Krivokapić <mijat2004@gmail.com>
2025-09-01 13:01:48 +0200

    Implement registration

--- frontend/cloud-computing25/src/app/app-routing.module.ts
+++ frontend/cloud-computing25/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from './layout/home/home.component';
+import {RegisterComponent} from './infrastructure/auth/register/register.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '**', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'register', component: RegisterComponent },
 ];
 
 @NgModule({
