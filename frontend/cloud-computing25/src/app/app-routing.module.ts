738d6a9b2e1091ddbbe68961ef476fac4e756ea6
Mijat Krivokapić <mijat2004@gmail.com>
2025-09-10 15:20:22 +0200

    Add artists page to navigation

--- frontend/cloud-computing25/src/app/app-routing.module.ts
+++ frontend/cloud-computing25/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import {HomeComponent} from './layout/home/home.component';
 import {RegisterComponent} from './infrastructure/auth/register/register.component';
 import {LoginComponent} from './infrastructure/auth/login/login.component';
 import {ArtistsComponent} from './artists/artists/artists.component';
+import {AuthGuard} from './infrastructure/auth/auth.guard';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'artists', component: ArtistsComponent},
+  {path: 'artists', component: ArtistsComponent, canActivate: [AuthGuard],
+    data: {role: ['admin']}},
 ];
 
 @NgModule({
