08b14e9b767a4a38842ddc97f6f7681cf9422eda
Milica Radic <milica.t.radic@gmail.com>
2025-09-12 18:33:24 +0200

    Add content module

---
+++ frontend/cloud-computing25/src/app/content/content.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+// Angular Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatChipsModule } from '@angular/material/chips'; // za mat-chip-grid
+import { MatDatepickerModule } from '@angular/material/datepicker'; // za mat-datepicker
+import { MatNativeDateModule } from '@angular/material/core'; // ili MatMomentDateModule
+import { MatTableModule } from '@angular/material/table'; // za mat-table
+import { MatIconModule } from '@angular/material/icon';
+
+import { UploadContentComponent } from './upload-content/upload-content.component';
+
+@NgModule({
+  declarations: [
+    UploadContentComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+
+    // Angular Material
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    MatDividerModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatTableModule
+  ],
+  exports: [UploadContentComponent]
+})
+export class ContentModule { }
