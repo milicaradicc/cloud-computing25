278a057abfc00156b77b840806a71c5dd530733d
Milica Radic <milica.t.radic@gmail.com>
2025-10-03 03:16:23 +0200

    Fix artist update

--- frontend/cloud-computing25/src/app/artists/create-artist/create-artist.component.ts
+++ frontend/cloud-computing25/src/app/artists/create-artist/create-artist.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { CreateArtistDto } from '../create-artist-dto.model';
+import { Artist } from '../artist.model';
 
 @Component({
   selector: 'app-create-artist',
@@ -9,7 +10,7 @@ import { CreateArtistDto } from '../create-artist-dto.model';
   templateUrl: './create-artist.component.html',
   styleUrls: ['./create-artist.component.css']
 })
-export class CreateArtistComponent {
+export class CreateArtistComponent implements OnInit {
   availableGenres: string[] = [
     'Pop', 'Rock', 'Hip Hop', 'Rap', 'Electronic', 
     'Classical', 'Jazz', 'Blues', 'Country', 'Reggae'
@@ -18,10 +19,23 @@ export class CreateArtistComponent {
   createForm: FormGroup = new FormGroup({
     name: new FormControl('', [Validators.required]),
     biography: new FormControl('', [Validators.required]),
-    genres: new FormControl([], [Validators.required]), // inicijalno prazan niz
+    genres: new FormControl([], [Validators.required]),
   });
 
-  constructor(public dialogRef: MatDialogRef<CreateArtistComponent>) {}
+  constructor(
+    public dialogRef: MatDialogRef<CreateArtistComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Artist | null
+  ) {}
+
+  ngOnInit(): void {
+    if (this.data) {
+      this.createForm.patchValue({
+        name: this.data.name,
+        biography: this.data.biography,
+        genres: this.data.genres
+      });
+    }
+  }
 
   onCancel(): void {
     this.dialogRef.close();
@@ -32,7 +46,7 @@ export class CreateArtistComponent {
       const artist: CreateArtistDto = {
         name: this.createForm.value.name,
         biography: this.createForm.value.biography,
-        genres: this.createForm.value.genres // već je niz
+        genres: this.createForm.value.genres
       };
       this.dialogRef.close(artist);
     }
